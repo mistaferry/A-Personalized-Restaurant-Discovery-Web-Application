168c77a4bf68528a935365da634fc3383c872698
mistaferry <irynka.huryn@gmail.com>
2024-04-05 13:50:10 +0200

    Add Vaadin and MainView

---
+++ src/main/java/ua/huryn/elasticsearch/MainView.java
@@ -0,0 +1,14 @@
+package ua.huryn.elasticsearch;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new Text("Welcome to MainView."));
+    }
+
+}
