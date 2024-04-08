e1b0888e6e039695e537b9a8061a87d65c2c6c75
mistaferry <irynka.huryn@gmail.com>
2024-04-08 16:13:43 +0200

    Add changes to Home page

--- src/main/java/ua/huryn/elasticsearch/MainView.java
+++ src/main/java/ua/huryn/elasticsearch/MainView.java
@@ -2,16 +2,21 @@ package ua.huryn.elasticsearch;
 
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.shared.InputField;
 import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import ua.huryn.elasticsearch.view.FavouriteView;
 import ua.huryn.elasticsearch.view.MenuView;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+import java.awt.*;
 
 public class MainView extends AppLayout {
 
@@ -21,7 +26,16 @@ public class MainView extends AppLayout {
 //        // Create a grid to display the data
 //
 //    }
-
+//
+//    <div class="col-auto">
+//      <label class="sr-only" for="inlineFormInputGroup">Username</label>
+//      <div class="input-group mb-2">
+//        <div class="input-group-prepend">
+//          <div class="input-group-text">@</div>
+//        </div>
+//        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+//      </div>
+//    </div>
 
     public MainView() {
         createHeader();
@@ -30,19 +44,13 @@ public class MainView extends AppLayout {
 
     private void createHeader() {
         H1 logo = new H1("Diplom Huryn");
-        logo.addClassNames(
-                LumoUtility.FontSize.LARGE,
-                LumoUtility.Margin.MEDIUM,
-                LumoUtility.TextColor.ERROR);
+//        TextField search = new TextField();
+//        search.setPlaceholder("Search");
+//        search.setClearButtonVisible(true);
+//        search.setPrefixComponent(VaadinIcon.SEARCH.create());
+//        search.setClassName("mx-auto");
 
-        var header = new HorizontalLayout(new DrawerToggle(), logo );
-
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        header.setWidthFull();
-        header.addClassNames(
-                LumoUtility.Padding.Vertical.NONE,
-                LumoUtility.Padding.Horizontal.MEDIUM);
-
+        var header = new HorizontalLayout(new DrawerToggle(), logo);
         addToNavbar(header);
 
     }
--- src/main/java/ua/huryn/elasticsearch/view/MenuView.java
+++ src/main/java/ua/huryn/elasticsearch/view/MenuView.java
@@ -1,34 +1,51 @@
 package ua.huryn.elasticsearch.view;
 
-import com.google.maps.errors.ApiException;
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.dependency.JsModule;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import ua.huryn.elasticsearch.MainView;
-import ua.huryn.elasticsearch.service.RestaurantService;
+import com.vaadin.flow.component.textfield.TextField;
+
+
+import java.awt.*;
 
 @PageTitle("Menu")
 @Route(value="", layout = MainView.class)
 @CssImport("styles.css")
+@StyleSheet("https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css")
 public class MenuView extends VerticalLayout {
 
     public MenuView() {
-        // Створення вертикальної секції №1
-        HorizontalLayout section1 = new HorizontalLayout();
-        section1.setAlignItems(FlexComponent.Alignment.CENTER);
-        VerticalLayout horizontalSection1 = new VerticalLayout();
-        horizontalSection1.add(new Text("Horizontal Section 1"));
-        horizontalSection1.addClassNames("left-hor-sec");
-        VerticalLayout horizontalSection2 = new VerticalLayout();
-        horizontalSection2.add(new Text("Horizontal Section 2"));
+        Div section1 = new Div();
+        section1.addClassNames("w-100 bg-warning p-2 mx-1 d-flex justify-content-center");
 
-        section1.add(horizontalSection1, horizontalSection2);
-        add(section1);
+        Div searchDiv = new Div();
+        TextField searchField = new TextField();
+        searchField.setPlaceholder("Search...");
+        searchField.setPrefixComponent(VaadinIcon.SEARCH.create());
+        searchField.addClassNames("bg-light p-2");
+        searchField.setMaxLength(50);
+        searchDiv.add(searchField);
+        section1.add(searchDiv);
+
+        Div section2 = new Div();
+        section2.addClassNames("d-flex flex-row", "w-100");
+
+        Div filtersDiv = new Div();
+        filtersDiv.add(new Text("hdjdl"));
+        filtersDiv.addClassNames("w-25 bg-success p-2 mx-1");
+
+        Div menuDiv = new Div();
+        menuDiv.add(new Text("uy76tre"));
+        menuDiv.addClassNames("w-75 bg-danger p-2");
+
+        section2.add(filtersDiv, menuDiv);
+        add(section1,section2);
     }
 }
